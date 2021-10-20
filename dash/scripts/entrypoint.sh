089ab79206ef3a24b9d226bada1a4be4bf6d5e2c
visimo-jarod <jarod@visimo.ai>
2021-10-20 09:17:33 -0400

    Updated the entrypoint script to handle initializing the app.py in the apps directory.

--- dash/scripts/entrypoint.sh
+++ dash/scripts/entrypoint.sh
@@ -3,12 +3,13 @@
 # expects your custom app.py to be mounted into /home/python3/apps, otherwise it
 # will run the provided app.py in /home/python3/app.py
 
-if [ "$(ls -A $HOME/apps)" ] 
+if [ -e $HOME/apps/app.py ] 
 then 
     echo "running mounted application.."
-    cd $HOME/apps
 else
-    cd $HOME
+    echo "pre-loading app.py"
+    cp $HOME/app.py apps/
 fi
 
+cd $HOME/apps
 python app.py
