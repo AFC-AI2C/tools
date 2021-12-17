3d168e376ff5cc2b8bc6456e195954ee09ac94dc
komnick <daniel.komnick@gmail.com>
2021-12-17 00:49:23 -0500

    update script

--- update_images.sh
+++ update_images.sh
@@ -1,25 +1,31 @@
 #! /bin/bash
 
-for file in */ ; do 
-    if [[ -d "$file" && ! -L "$file" ]]; then
-        #cat "./$file/Dockerfile"| head -n 1
-        # echo $file
-        #echo $file | tr '/' ' ' 
+IMAGES=./image-build-order.txt
+for image in $(cat < $IMAGES); do
+    eval "docker build -t 'afcai2c/$image' --no-cache './$image'"
+    eval "docker push 'afcai2c/$image:latest'"
 
-        # Replaces the slash at the end of the string
-        name=${file//[\/]/}
-
-        # Builds Docker images
-        eval "docker build -t 'afcai2c/$name' --no-cache './$name'"
-
-        # Commit update to github
-        # NOT NEEDED, MAY USE LATER...
-        # git add "./$name"
-        # git commit -m "Updated packages in $name"
-        # git push origin main
-
-        eval "docker push 'afcai2c/$name:latest'"
-    fi
 done
 
+# for file in */ ; do 
+#     if [[ -d "$file" && ! -L "$file" ]]; then
+#         #cat "./$file/Dockerfile"| head -n 1
+#         # echo $file
+#         #echo $file | tr '/' ' ' 
+
+#         # Replaces the slash at the end of the string
+#         name=${file//[\/]/}
+
+#         # Builds Docker images
+#         eval "docker build -t 'afcai2c/$name' --no-cache './$name'"
+
+#         # Commit update to github
+#         # NOT NEEDED, MAY USE LATER...
+#         # git add "./$name"
+#         # git commit -m "Updated packages in $name"
+#         # git push origin main
+
+#         eval "docker push 'afcai2c/$name:latest'"
+#     fi
+# done
 
