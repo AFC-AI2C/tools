daadff069c4b6286c5ab744cb808609bf4b2a06f
Daniel Komnick <daniel.s.komnick.mil@army.mil>
2022-04-08 15:25:57 -0400

    added jlab-r

--- r-studio-geo/scripts/ai-ml-packages.R
+++ r-studio-geo/scripts/ai-ml-packages.R
@@ -8,7 +8,7 @@ for (package in packagesToInstall) {
 #    install.packages(package, dependencies=TRUE, repos='http://cran.us.r-project.org');
     install.packages(package, dependencies=TRUE, repos='https://packagemanager.rstudio.com/all/latest');
 
-    # Installation from local repo
+#    # Installation from local repo
 #    install.packages(package, dependencies=TRUE, repos=NULL, recursive=TRUE, contriburl='file://tmp/repo/')
     if ( ! library(package, character.only=TRUE, logical.return=TRUE) ) {
         quit(status=1, save='no')
